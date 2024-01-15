6c2da05334a1ea0925e257e64816fd470ad88357
JP <jp.engelbrecht@pushsecurity.com>
2024-01-15 11:43:49 +0200

    nuxt image not working with predefined images

--- app.vue
+++ app.vue
@@ -4,5 +4,16 @@
   </div>
 </template>
 <script setup>
-defineOgImageComponent('Default')
+// does not work
+// defineOgImageComponent('Default',  {
+//   url: "https://images.ctfassets.net/y1cdw1ablpvd/2G9eUljiJXW11zOTpqRSUk/0e41dc83190b8be5d6ea8ec58d83da90/blog_image.png",
+//   heigh: 1008,
+//   width: 1796,
+
+// })
+
+// doest not work either
+defineOgImage({
+  url: 'https://images.ctfassets.net/y1cdw1ablpvd/2G9eUljiJXW11zOTpqRSUk/0e41dc83190b8be5d6ea8ec58d83da90/blog_image.png'
+})
 </script>
---
+++ components/OgImage/Default.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+// inherited attrs can mess up the satori parser
+defineOptions({
+  inheritAttrs: false,
+});
+
+// const props = defineProps({
+//   url: {
+//     type: String,
+//     default: "Push Security",
+//   },
+// });
+withDefaults(
+  defineProps<{
+    url?: string;
+    width?: number;
+    height?: number;
+  }>(),
+  {
+    url: null,
+  },
+);
+</script>
+
+<template>
+  <div class=" flex flex-col justify-center bg-[#212121]">
+ 
+    <img  :src="url" :height="height" :width="width" />
+    <!-- <img class="w-[900px] pl-[100px]" :src="url" /> -->
+  </div>
+</template>
